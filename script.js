60598b0f7faa637f823fa9558b007ceb35e20122
Prakhar Shrivastava <prakharshrivastava971@gmail.com>
2021-01-11 02:44:53 +0530

    dark/light mode, searching of characters and hamburger menu integrated

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // DOM Elements
 const charactersContainer_ = document.querySelector("#characters-container");
 const noSearchedCharacter_ = document.querySelector(".no-searched-character");
+const document_ = document.documentElement.style;
 
 // Characters
 var HPCharacters = [];
 
+// Funtion Definitions
 async function getHPCharacters() {
   await fetch("http://hp-api.herokuapp.com/api/characters")
     .then((response) => response.json())
@@ -63,5 +65,60 @@ function skeletonLoading() {
   for (let i = 0; i < 25; i++) charactersContainer_.innerHTML += skeletonStr;
 }
 
+function openHamburgerMenu(event) {
+  const { tagName } = event.target;
+
+  const clickedBtn = tagName === "DIV" ? event.target : event.target.parentNode;
+
+  clickedBtn.classList.toggle("maximize");
+}
+
+function enableDarkMode(event) {
+  const { tagName } = event.target;
+  const btn = tagName === "BUTTON" ? event.target : event.target.parentNode;
+
+  const isDarkEnabled = btn.classList.contains("off");
+
+  btn.children[1].innerText = isDarkEnabled ? "Light Mode" : "Dark Mode";
+
+  btn.children[0].style.left = isDarkEnabled ? "75%" : "0%";
+
+  // Enable Dark Mode
+  if (isDarkEnabled) {
+    btn.classList.remove("off");
+    setDarkModeColor();
+  } else {
+    btn.classList.add("off");
+    setLightModeColor();
+  }
+}
+
+function setDarkModeColor() {
+  document_.setProperty("--background", "#09092e");
+  document_.setProperty("--text-color", "#ffffff");
+  document_.setProperty(
+    "--text-bg-img",
+    "url(https://images.unsplash.com/photo-1508020963102-c6c723be5764?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80)"
+  );
+  document_.setProperty("--image-bg", "rgb(37, 36, 36)");
+  document_.setProperty("--card-shadow-color", "rgba(0,0,0,0.15");
+  document_.setProperty("--card-background", "#000000e5");
+  document_.setProperty("--card-border-color", "rgba(255,255,255,.2)");
+}
+
+function setLightModeColor() {
+  document_.setProperty("--background", "#ffffff96");
+  document_.setProperty("--text-color", "#000000");
+  document_.setProperty(
+    "--text-bg-img",
+    "url(https://images.unsplash.com/photo-1550757750-4ce187a65014?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=334&q=80)"
+  );
+  document_.setProperty("--image-bg", "rgb(229, 228, 231)");
+  document_.setProperty("--card-shadow-color", "rgba(143, 141, 141, 0.15)");
+  document_.setProperty("--card-background", "#ffffff");
+  document_.setProperty("--card-border-color", "rgba(0, 0, 0, 0.2)");
+}
+
+// Funtion Calls
 skeletonLoading();
 getHPCharacters();
